c7533012fc11926550ad19e3511b03a1acc157c6
Naring <0224kjh@gmail.com>
2025-02-07 09:31:08 +0900

    Player 캐릭터

---
+++ Window11_TextRPG/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Window11_TextRPG
+{
+    public class Player
+    {
+        public string name { get; set; } = "";
+        public int level { get; set; } = 1;
+        public string job { get; set; } = "전사";
+        public int atk { get; set; } = 10;
+        public int def { get; set; } = 5;
+        public int hp { get; set; } = 100;
+        public int gold { get; set; } = 1500;
+        // public Item weapon { get; set; } = null;
+        // public Item armor { get; set; } = null;
+
+        public bool Hpcheck()
+        {
+            if (hp <= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
